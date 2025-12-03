c5cd2a658544942dd78c2380ceb075a760d0fd76
JPS6847 <146823181+JPS6847@users.noreply.github.com>
2025-12-03 15:46:46 +0100

    Create cambiar_foto.js

---
+++ static/js/cambiar_foto.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const fotoInput = document.getElementById('fotoPerfilInput');
+        const preview = document.getElementById('preview');
+
+        fotoInput.addEventListener('change', function() {
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    preview.src = e.target.result;
+                }
+                reader.readAsDataURL(file);
+            }
+        });
